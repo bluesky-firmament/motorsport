88de7a52a4858aed7099e67f67f7752b6dbb447f
reuscaelum <reuscaelum@gmail.com>
2021-04-29 20:06:12 +0900

    NaNのところを改善

--- 2021/F1/plot.py
+++ 2021/F1/plot.py
@@ -4,39 +4,46 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
 import numpy as np
-
+import math
 
 def plot(drivername,driver_lap,lap_number,round):
     fig = plt.figure(figsize=(12, 8))
     npdriver_lap = np.array(driver_lap)
     npnumber_lap = np.array(lap_number)
-    c_min, c_max = np.percentile(npdriver_lap, [0, 80])
-    print(c_min,c_max,npdriver_lap.min())
-    npdriver_lap = np.clip(npdriver_lap, c_min, c_max)
-    input_driver_data = npdriver_lap.reshape(-1,1)
-    input_lap_data = npnumber_lap.reshape(-1,1)
-    # print(input_lap_data)
-    model = LinearRegression()
-    model.fit(input_lap_data, input_driver_data)
-    intercept = str(model.intercept_)
-    coef = str(model.coef_)
-    predicted = model.predict(input_lap_data)
-    plt.scatter(lap_number,driver_lap)
-    plt.plot(input_lap_data, predicted, color = 'blue')
-    title = drivername + "_Race_Pace"
-    plt.title(title)
-    formula = "y="+ coef + "+" + intercept
-    plt.text(40, 100, formula, size=10)
-    plt.ylim(60.0,120.0)
-    figure_name = "Rd" + str(round) + "/" + drivername + "_Race_Pace.png"
-    plt.savefig(figure_name)
-    # plt.show()
+    try:
+        npdriver_lap = np.nan_to_num(npdriver_lap, nan=np.nanmean(npdriver_lap))
+        c_min, c_max = np.percentile(npdriver_lap, [0, 80])
+        print(c_min,c_max,npdriver_lap.min())
+        npdriver_lap = np.clip(npdriver_lap, c_min, c_max)
+        input_driver_data = npdriver_lap.reshape(-1,1)
+        input_lap_data = npnumber_lap.reshape(-1,1)
+        # print(input_lap_data)
+        model = LinearRegression()
+        model.fit(input_lap_data, input_driver_data)
+        intercept = str(model.intercept_)
+        coef = str(model.coef_)
+        predicted = model.predict(input_lap_data)
+        plt.scatter(lap_number,driver_lap)
+        plt.plot(input_lap_data, predicted, color = 'blue')
+        title = drivername + "_Race_Pace"
+        plt.title(title)
+        formula = "y="+ coef + "+" + intercept
+        plt.text(40, 100, formula, size=10)
+        plt.ylim(60.0,120.0)
+        figure_name = "Rd" + str(round) + "/" + drivername + "_Race_Pace.png"
+        plt.savefig(figure_name)
+        # plt.show()
+    except (ValueError,IndexError):
+        return
+    
 
 
-f = open('Rd2_Race_1_total.csv','r')
-reader = csv.reader(f)
+
 print("ラウンド数を入れてください")
 round = int(input())
+file_name = 'Rd' + str(round) + '_Race_1_total.csv'
+f = open(file_name,'r')
+reader = csv.reader(f)
 lap_number=[]
 drivername = []
 lap_time = []
@@ -53,7 +60,7 @@ for row in reader:
             try:
                 lap_time.append(float(lap))
             except (ValueError,IndexError):
-                lap_time.append(0.0)
+                lap_time.append(math.nan)
         # print(len(lap_time))
         # print(len(lap_number))
         plot(row[0],lap_time,lap_number,round)
